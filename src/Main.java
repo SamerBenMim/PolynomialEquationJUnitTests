239c609f4d9457e3b1c500b1789587448cda36dc
SamerBenMim <samer.benmim@insat.ucar.tn>
2023-05-01 00:12:14 +0100

    testing SecondDegreeEquations

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+import junit.monprojet.PolynomeSecondDegre;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println('e');
+    }
+}
---
+++ src/junit/monprojet/test/TestEquationSecondDegre.java
@@ -0,0 +1,31 @@
+import junit.monprojet.PolynomeSecondDegre;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TestEquationSecondDegre {
+
+    @Test
+    public void test2Racines() {
+        PolynomeSecondDegre p = new PolynomeSecondDegre(1, 1, -2);
+        double[] racines = p.calculRacines();
+        assertEquals(2, racines.length);
+        assertEquals(1, racines[0], 0.0001);
+        assertEquals(-2, racines[1], 0.0001);
+    }
+
+    @Test
+    public void test1Racine() {
+        PolynomeSecondDegre p = new PolynomeSecondDegre(1, -2, 1);
+        double[] racines = p.calculRacines();
+        assertEquals(1, racines.length);
+        assertEquals(1, racines[0], 0.0001);
+    }
+
+    @Test
+    public void testPasDeRacineReelle() {
+        PolynomeSecondDegre p = new PolynomeSecondDegre(1, 1, 1);
+        double[] racines = p.calculRacines();
+        assertNull(racines);
+    }
+}
